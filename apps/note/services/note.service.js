4f35e0e786acb61c791d72751bea855f1131649e
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2022-12-28 12:20:36 +0200

    initial service file

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,161 @@
-console.log('Hi')
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+const NOTES_KEY = 'notesDB'
+let gFilterBy = { type: '' }
+
+export const keepService = {
+    // getNotes,
+    query,
+    getNoteById,
+    removeNoteById,
+    save,
+    getEmptyNote,
+    getNextNoteId,
+    getPrevNoteId,
+    setFilterBy,
+    getFilterBy,
+}
+
+_createNotes()
+
+function query() {
+    return storageService.query(NOTES_KEY)
+        .then(notes => {
+            if (gFilterBy.type) {
+                // const regex = new RegExp(gFilterBy.txt, 'i')
+                notes = notes.filter(note => note.type = gFilterBy.type)
+            }
+            // if (gFilterBy.minScore) {
+            //     notes = notes.filter(note => note.score >= gFilterBy.minScore)
+            // }
+            return notes
+        })
+}
+
+
+function getNoteById(id) {
+    return storageService.get(NOTES_KEY, id)
+}
+
+function removeNoteById(id) {
+    return storageService.remove(NOTES_KEY, id)
+}
+
+function save(note) { //add if not empty?
+    if (note.id) {
+        return storageService.put(NOTES_KEY, note) //change to update?
+    } else {
+        return storageService.post(NOTES_KEY, note)
+    }
+}
+
+function getEmptyNote(type = 'note-txt', backgroundColor = '#ffffff') {
+    const emptyItem = _setInfoType(type)
+    // return { id: '', vendor, maxSpeed }
+    return { id: '', type, info, backgroundColor }
+}
+
+function _setInfoType(type) {
+    let emptyItem = {}
+    switch (type) {
+        case 'note-txt':
+            emptyItem = { txt: '' }
+            break
+        case 'note-img':
+            emptyItem = { url: '' }
+            break
+        case 'note-todos':
+            emptyItem = { todos: [getNewTodo()] }
+            break
+    }
+    return emptyItem
+}
+
+function getNewTodo() {
+    return emptyTodo = { txt: '', done: null, id: utilService.makeId() }
+}
+
+function getNextNoteId(noteId) {
+    return storageService.query(NOTES_KEY)
+        .then(notes => {
+            var idx = notes.findIndex(note => note.id === noteId)
+            if (idx === notes.length - 1) idx = -1
+            return notes[idx + 1].id
+        })
+}
+
+function getPrevNoteId(noteId) {
+    return storageService.query(NOTES_KEY)
+        .then(notes => {
+            let idx = notes.findIndex(note => note.id === noteId)
+            if (idx === 0) idx = notes.length - 0
+            return notes[idx - 1].id
+        })
+}
+
+function getFilterBy() {
+    return { ...gFilterBy }
+}
+
+function setFilterBy(filterBy = {}) {
+    if (filterBy.type !== undefined) gFilterBy.type = filterBy.type
+    // if (filterBy.minScore !== undefined) gFilterBy.minScore = filterBy.minScore
+    return gFilterBy
+}
+
+function _save(entityType, entities) {
+    localStorage.setItem(entityType, JSON.stringify(entities))
+}
+
+function _createDemoKeepNotes() {
+
+    const notes = [
+        {
+            id: "n101",
+            type: "note-txt",
+            isPinned: true,
+            info: {
+                txt: "Fullstack Me Baby!"
+            }
+        },
+        {
+            id: "n102",
+            type: "note-img",
+            info: {
+                url: "http://some-img/me",
+                title: "Bobi and Me"
+            },
+            style: {
+                backgroundColor: "#00d"
+            }
+        },
+        {
+            id: "n103",
+            type: "note-todos",
+            info: {
+                label: "Get my stuff together",
+                todos: [
+                    { txt: "Driving liscence", doneAt: null },
+                    { txt: "Coding power", doneAt: 187111111 }
+                ]
+            }
+        }
+    ]
+
+    utilService.saveToStorage(NOTES_KEY, notes)
+
+}
+
+function _createNotes() {  //takes notes from sotrage. if there aren't any. create and add to sotrage.
+
+    let notesDB = utilService.loadFromStorage(NOTES_KEY)
+    if (!notesDB || !notesDB.length) {
+        _createDemoKeepNotes()
+        // _save(NOTES_KEY, notesDB)
+        // return notesDB
+    }
+
+}
+
+
--- services/util.service.js
+++ services/util.service.js
@@ -6,6 +6,8 @@ export const utilService = {
     padNum,
     getDayName,
     getMonthName,
+    saveToStorage,
+    loadFromStorage
 }
 
 function makeId(length = 6) {
@@ -60,3 +62,14 @@ function getMonthName(date) {
     ]
     return monthNames[date.getMonth()]
 }
+
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
+
