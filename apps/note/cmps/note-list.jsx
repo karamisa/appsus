addda5c47f2e5f974e4814503a27ec3b8b9e829d
Karam Isa <karam_isa@icloud.com>
2022-12-29 21:05:58 +0200

    minor tweak to notelist

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -3,12 +3,12 @@ import { NotePreview } from "./note-preview.jsx"
 
 export function NoteList({ notes, onRemoveNote, onNoteClicked, saveChanges}) {
 
-    return <ul className="note-list full">
+    return <section className="notes-list full">
     {
         notes.map(note => 
             <NotePreview key={note.id} note={note} onRemoveNote={onRemoveNote} onNoteClicked={onNoteClicked} saveChanges={saveChanges}/>       
         )
     }
-</ul>
+</section>
 
 }
