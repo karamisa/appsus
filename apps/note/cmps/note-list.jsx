a3c485d22b68ef3725d09cd8e76fac2179c06f2f
Karam Isa <karam_isa@icloud.com>
2022-12-29 21:04:43 +0200

    dfdfsd

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -3,7 +3,7 @@ import { NotePreview } from "./note-preview.jsx"
 
 export function NoteList({ notes, onRemoveNote, onNoteClicked, saveChanges}) {
 
-    return <ul className="note-list grid full">
+    return <ul className="note-list full">
     {
         notes.map(note => 
             <NotePreview key={note.id} note={note} onRemoveNote={onRemoveNote} onNoteClicked={onNoteClicked} saveChanges={saveChanges}/>       
