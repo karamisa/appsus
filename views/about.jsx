61efa20d094a38a6be15ddd11f3d7d9b649ac2e6
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2022-12-30 16:42:29 +0200

    about

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,26 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <div className="karam team">
+            <img src="../assets/img/karam.jpg" alt="Karam" />
+            <div className="about-container">
+                <span className="name">Karam Isa</span>
+                <span>Born and raised in the United States,</span>
+                <span>I studied at the University of Kentucky where I obtained a degree in Chemical Engineering.</span>
+                <span>Following univeristy I moved to Israel where I have began a new chapter of my life across the world.</span>
+                <span>I am currently learning Hebrew and Javascript with the goal of being a fullstack developer.</span>
+                <span>I love to excercise and lift weights as well as enjoy the beuatiful beach!</span>
+            </div>
+        </div>
+        <div className="elichay team">
+            <img src="../assets/img/elichay.jpg" alt="Elichay" />
+            <div className="about-container">
+                <span className="name">Elichay Kaplan</span>
+                <span>Born and raised in the Israel,</span>
+                <span>I studied at Bar-Ilan University, where I obtained my Economics degree.</span>
+                <span>Worked in one of the world's major banks for over 12 years as a Credit Operations Manager.</span>
+                <span>I am currently learning Javascript with the goal of being a fullstack developer.</span>
+            </div>
+        </div>
     </section>
 }
