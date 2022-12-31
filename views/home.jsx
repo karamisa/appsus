21c02f75df04c90357a894fd4c6c43a071e1f13b
Karam Isa <karam_isa@icloud.com>
2022-12-31 22:05:37 +0200

    minor fix (final submission)

--- views/home.jsx
+++ views/home.jsx
@@ -3,7 +3,7 @@ const { Fragment } = React
 export function Home() {
 
     return <Fragment>
-         <img src='../assets/img/bg-img.jpg' className='bg-img' />
+         <img src='assets/img/bg-img.jpg' className='bg-img' />
         <section className="home">
             <h1 className="welcome">Appsus</h1>
             <h2>Orgenaize Simplicity Efficiency</h2>
